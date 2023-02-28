34cb9e1fa0be2e19ac9f4ccd5ff82c02c187dc00
edufardo <eduardo.larraz.labordeta@gmail.com>
2023-02-28 01:41:03 +0100

    UPD - Pract

--- 1.Deploy-spring-app/src/main/java/es/codeurjc/mca/events/image/S3ImageService.java
+++ 1.Deploy-spring-app/src/main/java/es/codeurjc/mca/events/image/S3ImageService.java
@@ -1,20 +1,55 @@
 package es.codeurjc.mca.events.image;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectAclResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service("storageService")
 @Profile("production")
 public class S3ImageService implements ImageService {
 
+    public static S3Client s3;
+
+    public S3ImageService() {
+        s3 = S3Client.builder()
+                .region(Region.EU_WEST_1)
+                .build();
+    }
+
+    private String bucketName = "mca.practicas";
+
     @Override
     public String createImage(MultipartFile multiPartFile) {
-       return null;
+        String fileName = multiPartFile.getOriginalFilename();
+        File file = new File(System.getProperty("java.io.tmpdir")+"/"+fileName);
+        //multiPartFile.transferTo(file);
+        s3.putObject(
+            PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build(), 
+            RequestBody.fromFile(file)
+        );
+        return fileName;
     }
 
     @Override
     public void deleteImage(String image) {
+        s3.deleteObject(
+                DeleteObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(image)
+                        .build());
 
     }
 
