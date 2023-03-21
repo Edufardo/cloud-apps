f9ac18cfa8b82b9e7836431ba7373db1b25f7b8f
edufardo <eduardo.larraz.labordeta@gmail.com>
2023-03-21 01:30:16 +0100

    WIP - Docker

--- 1.Contenedores-dockerizar-app/toposervice/src/main/java/es/codeurjc/mastercloudapps/topo/Application.java
+++ 1.Contenedores-dockerizar-app/toposervice/src/main/java/es/codeurjc/mastercloudapps/topo/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
